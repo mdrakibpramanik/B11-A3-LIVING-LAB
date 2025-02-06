af9a360c19081b3b3895d024e80d92c66b940d7b
RAKIBUL ISLAM <mrprakibulislam03@gmail.com>
2025-02-06 23:01:41 +0600

    added new images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+{import('tailwindcss').Config}
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
@@ -6,3 +6,5 @@ module.exports = {
     },
     plugins: [],
   }
+
+  
